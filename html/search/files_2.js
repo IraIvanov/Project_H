2ac0a0298b612b967d27c38cd0573cde7b723694
Ivanov Roman (Ira) <100772290+IraIvanov@users.noreply.github.com>
2022-09-08 19:35:16 +0300

    Added documantation

---
+++ html/search/files_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['quad_5feq_2ecpp_14',['quad_eq.cpp',['../quad__eq_8cpp.html',1,'']]]
+];
---
+++ html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['input_18',['input',['../io_8cpp.html#a34e8a7d6e2ff8f34a5d2e86b3865cba2',1,'io.cpp']]]
+];
